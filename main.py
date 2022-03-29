f4f791c09e4f8cdacce4338d67a7a7dae952365a
Bernard Rouhi <Bernardrouhi@users.noreply.github.com>
2022-03-29 14:51:20 +0800

    feature up

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ from widgets.handsFreeMainWindow import HandsFreeMainWindow
 #Widget
 class HFWidget(QWidget):
 	instances = list()
-	TITLE = "HandsFree v2.1.1-BETA"
+	TITLE = "HandsFree v2.2.0-BETA"
 	def __init__(self, parent=get_MayaWindow(), projectfile=str(), edit=bool(False),  *args, **kwargs):
 		super(HFWidget, self).__init__(parent=parent, *args, **kwargs)
 		self.setMinimumWidth(400)
